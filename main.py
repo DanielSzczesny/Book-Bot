09bada34a883e6761e2f004cdba4c744ed9170ad
DanielSzczesny <daniel.szcz3sny@gmail.com>
2025-01-29 13:54:59 +0100

    task 10 boot.dev project

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-
 def count_words(text):
     result = text.split()
     return len(result)
+
+
+def count_each_letter(text):
+    text_lowercase = text.lower()
+    result = {}
+    for i in range(0, len(text_lowercase)):
+        if text_lowercase[i] in result:
+            result[text_lowercase[i]] += 1
+        else:
+            result[text_lowercase[i]] = 1
+    return result
+
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(count_words(file_contents))
+        print(count_each_letter(file_contents))
+
 
 main()
