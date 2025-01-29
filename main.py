9dc15be2a3577b0faa18b3b189b2c90c222bd21d
DanielSzczesny <daniel.szcz3sny@gmail.com>
2025-01-29 16:11:04 +0100

    Task 11 boot.dev

--- main.py
+++ main.py
@@ -14,11 +14,35 @@ def count_each_letter(text):
     return result
 
 
+def sort_on(dictionary):
+    return dictionary["num"]
+
+
+def count_each_alpha_char(text):
+    text_lowercase = text.lower()
+    result = []
+    for i in range(0, len(text_lowercase)):
+        if text_lowercase[i].isalpha():
+            if len(result) == 0:
+                result.append({"letter": text_lowercase[i], "num": 1})
+            else:
+                added = False
+                for j in range(0, len(result)):
+                    if text_lowercase[i] == result[j]["letter"]:
+                        result[j]["num"] += 1
+                        added = True
+                        break
+                if not added:
+                    result.append({"letter": text_lowercase[i], "num": 1})
+    return result
+
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(count_words(file_contents))
-        print(count_each_letter(file_contents))
+        sorted_dict = count_each_alpha_char(file_contents)
+        sorted_dict.sort(reverse=True, key=sort_on)
+        print(sorted_dict)
 
 
 main()
